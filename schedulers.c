289fc1d552fc47258d3deab017190af41198bfd8
miguel5 <miguel.amigot@gmail.com>
2014-09-30 00:04:01 -0400

    Added readers for the processes and main data structure

--- schedulers.c
+++ schedulers.c
@@ -2,33 +2,80 @@
 #include <stdlib.h>
 
 
-FILE *fpRandomNumbers;
-FILE *fpInput;
+typedef struct {
+	int A, B, C, IO;
+} Process;
+
+
+void printProcesses();
+
+static FILE *fpRandomNumbers;
+static FILE *fpInput;
+
+static int numProcesses;
+static Process *processes;
+
+
+Process *Process_create(int A, int B, int C, int IO){
+	Process *proc = malloc(sizeof(Process));
+
+	proc->A = A;
+	proc->B = B;
+	proc->C = C;
+	proc->IO = IO;
+
+	return proc;
+}
 
 int randomOS(int u){
 	int curr;
-	fscanf(fpRandomNumbers, "%d", &curr);
+	if(fpRandomNumbers != NULL){
+		fscanf(fpRandomNumbers, "%d", &curr);
+		return 1 + (curr % u);
+	}
 
-	return 1 + (curr % u);
+	return -1;
 }
 
-int readInput(){
-	// The first integer of the input gives the total number of processes that must be read
-	int numProcesses;
+void readInput(){
+	// The first integer of the file gives the total number of processes
 	fscanf(fpInput, "%d", &numProcesses);
 
-	return numProcesses;
+	// Create the array of processes
+	processes = malloc(sizeof(Process) * numProcesses);
+
+	int A, B, C, IO;
+	Process curr;
+	for(int i = 0; i < numProcesses; i++){
+		fscanf(fpInput, " (%d %d %d %d) ", &A, &B, &C, &IO);
+		curr = *Process_create(A, B, C, IO);
+		processes[i] = curr;
+	}
+
+	printProcesses();
+}
+
+void printProcesses(){
+
+	Process curr;
+	for(int i = 0; i < numProcesses; i++){
+		curr = processes[i];
+		printf("(%d %d %d %d)\n", curr.A, curr.B, curr.C, curr.IO);
+	}
 }
 
 int main(int argc, char *argv[]){
 
 	fpRandomNumbers = fopen("random-numbers.txt", "r");
-	fpInput = fopen("inputs/input-7.txt", "r");
+	fpInput = fopen("inputs/input-4.txt", "r");
 
-	//printf("%d\n", randomOS(591));
-	//printf("%d\n", randomOS(132));
+	readInput();
 
-	printf("%d\n", readInput());
 
+
+	fclose(fpRandomNumbers);
+	fclose(fpInput);
+
+	free(processes);
 
 }
