8d9f19154608d64adfa5efc36811be527ed66d01
miguel5 <miguel.amigot@gmail.com>
2014-10-11 22:37:32 -0400

    Sort processes at beginning using Selection Sort

--- schedulers.c
+++ schedulers.c
@@ -30,6 +30,16 @@ Process *Process_create(int A, int B, int C, int IO){
 	return proc;
 }
 
+int randomOS(int u){
+	int curr;
+	if(fpRandomNumbers != NULL){
+		fscanf(fpRandomNumbers, "%d", &curr);
+		return 1 + (curr % u);
+	}
+
+	return -1;
+}
+
 void readInput(){
 	// The first integer of the file gives the total number of processes
 	fscanf(fpInput, "%d", &numProcesses);
@@ -40,12 +50,43 @@ void readInput(){
 	int A, B, C, IO;
 	Process curr;
 	for(int i = 0; i < numProcesses; i++){
-		fscanf(fpInput, " (%d %d %d %d) ", &A, &B, &C, &IO);
+		fscanf(fpInput, " ( %d %d %d %d ) ", &A, &B, &C, &IO);
 		curr = *Process_create(A, B, C, IO);
 		processes[i] = curr;
 	}
+	
 
-	printProcesses();
+	printf("The original input was: %d ", numProcesses);
+	for(int i = 0; i < numProcesses; i++){
+		curr = processes[i];
+		printf("(%d %d %d %d) ", curr.A, curr.B, curr.C, curr.IO);
+	}
+	printf("\n");
+}
+
+void sortProcessesByArrivalTime(){
+	// Selection sort
+
+	Process proc;
+	int i, j, minIndex;
+	for(i = 0; i < numProcesses; i++){
+		minIndex = i;
+		for(j = i + 1; j < numProcesses; j++)
+			if(processes[j].A < processes[minIndex].A)
+				minIndex = j;
+
+		proc = processes[i];
+		processes[i] = processes[minIndex];
+		processes[minIndex] = proc;
+	}
+
+
+	printf("The (sorted) input is:  %d ", numProcesses);
+	for(int i = 0; i < numProcesses; i++){
+		proc = processes[i];
+		printf("(%d %d %d %d) ", proc.A, proc.B, proc.C, proc.IO);
+	}
+	printf("\n\n");
 }
 
 void printProcesses(){
@@ -57,24 +98,29 @@ void printProcesses(){
 	}
 }
 
-int randomOS(int u){
-	int curr;
-	if(fpRandomNumbers != NULL){
-		fscanf(fpRandomNumbers, "%d", &curr);
-		return 1 + (curr % u);
+void printOutput(){
+
+	printf("The original input was: %d ", numProcesses);
+
+	Process curr;
+	for(int i = 0; i < numProcesses; i++){
+		curr = processes[i];
+		printf("(%d %d %d %d) ", curr.A, curr.B, curr.C, curr.IO);
 	}
 
-	return -1;
+	printf("\n");
+
+
 }
 
 int main(int argc, char *argv[]){
 
 	fpRandomNumbers = fopen("random-numbers.txt", "r");
-	fpInput = fopen("inputs/input-4.txt", "r");
+	fpInput = fopen("inputs/input-5.txt", "r");
 
 	readInput();
 
-
+	sortProcessesByArrivalTime();
 
 	fclose(fpRandomNumbers);
 	fclose(fpInput);
